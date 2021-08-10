8c15e64db49d1a433725921b1590778d7d9b8c56
Dries Van De Putte <dvandeputte@stsci.edu>
2021-08-10 18:51:34 -0400

    Refactor to use dataclasses instead of dicts

--- resample_and_merge_spitzer_cubes.py
+++ resample_and_merge_spitzer_cubes.py
@@ -23,6 +23,37 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from photutils.aperture import SkyRectangularAperture, aperture_photometry
 import reproject
+from collections import namedtuple
+from dataclasses import dataclass
+
+
+@dataclass
+class Cube:
+    """Fixed pattern for the information in one cube that we want to pass
+    around
+
+    """
+
+    file_handle: fits.HDUList
+    data: np.ndarray
+    wavelength: np.ndarray
+    wcs: WCS
+
+
+@dataclass
+class CubeSet:
+    """The four cubes we have for each object
+
+    Constructor is automatically generated"""
+
+    ll1: Cube
+    ll2: Cube
+    sl1: Cube
+    sl2: Cube
+
+    def all_cubes(self):
+        """You can add functions to this type of class like this"""
+        return [self.ll1, self.ll2, self.sl1, self.sl2]
 
 
 def read_spitzer_cube(fn):
@@ -31,13 +62,12 @@ def read_spitzer_cube(fn):
 
     Returns
     -------
-    dict : dict with 'cube' = Primary HDU, 'wavelength' = wavelength
-        table, 'wcs' = WCS
+    Cube
     """
-    cube = fits.open(fn)[0]
-    wavs = Table.read(fn)["WAVELENGTH"][0]
+    hdulist = fits.open(fn)
+    wavs = Table.read(fn)["WAVELENGTH"][0].flatten()
     wcs = WCS(fn, naxis=2)
-    return dict(filename=fn, cube=cube, wavelength=wavs, wcs=wcs)
+    return Cube(file_handle=hdulist, data=hdulist[0].data, wavelength=wavs, wcs=wcs)
 
 
 def get_SAGE_cubes(target, uncertainty=False):
@@ -47,10 +77,11 @@ def get_SAGE_cubes(target, uncertainty=False):
 
     Returns
     -------
-    cubes : list of dicts (see read_spitzer_cube for format)
+    CubeSet
     """
     dname = f"data/sage-spec_{target}_4dec08"
     d = Path(dname)
+    # load them in the right order...
     cube_names = ["ll_LL1", "ll_LL2", "sl_SL1", "sl_SL2"]
     cubes = []
     for cube_name in cube_names:
@@ -60,20 +91,21 @@ def get_SAGE_cubes(target, uncertainty=False):
             fname = fname.replace("cube.fits", "cube_unc.fits")
         cubes.append(read_spitzer_cube(str(d / fname)))
 
-    return cubes
+    # ... in the future we can unambiguously access them here
+    return CubeSet(*cubes)
 
 
-def quicklook_cubes(cube_dicts, apertures=None):
-    """Display 4 cubes and a set of apertures in a 2x2 grid."""
+def quicklook_cubes(cube_set, apertures=None):
+    """Display the 4 cubes and a set of apertures in a 2x2 grid."""
     fig = plt.gcf()
-    for i, cube in enumerate(cube_dicts):
-        ax = fig.add_subplot(2, 2, i + 1, projection=cube["wcs"])
-        ax.imshow(cube["cube"].data[-1])
+    for i, cube in enumerate(cube_set.all_cubes()):
+        ax = fig.add_subplot(2, 2, i + 1, projection=cube.wcs)
+        ax.imshow(cube.data[-1])
         ax.grid()
         ax.coords[0].set_format_unit(u.degree, decimal=True)
         ax.coords[1].set_format_unit(u.degree, decimal=True)
         if apertures is not None:
-            apertures.to_pixel(cube["wcs"]).plot(axes=ax, color="r")
+            apertures.to_pixel(cube.wcs).plot(axes=ax, color="r")
 
 
 # the wcs we want our final map to have
@@ -103,7 +135,7 @@ def make_ra_dec_wcs(center_ra, center_dec, pix_angle_delta, npix_ra, npix_dec):
 
 
 def reproject_and_merge_cubes(
-    cube_dicts, center_ra, center_dec, pix_angle_delta, npix_ra, npix_dec, filename=None
+    cube_set, center_ra, center_dec, pix_angle_delta, npix_ra, npix_dec, filename=None
 ):
     """
     Reproject all four cubes onto pixel grid wcs, and merge the result.
@@ -112,18 +144,19 @@ def reproject_and_merge_cubes(
     output_projection = make_ra_dec_wcs(
         center_ra, center_dec, pix_angle_delta, npix_ra, npix_dec
     )
-    wavtables = [d["wavelength"] for d in cube_dicts]
-    output_wavs = np.concatenate(wavtables).flatten()
+    output_wavs = np.concatenate([c.wavelength for c in cube_set.all_cubes()]).flatten()
     nw = len(output_wavs)
 
     ny, nx = npix_dec, npix_ra
     output_cube_array = np.zeros((nw, ny, nx))
 
     start = 0
-    for d in cube_dicts:
-        stop = start + len(d["wavelength"])
-        output_cube_array[start:stop] = reproject_cube(d, output_projection, ny, nx)
+    for c in cube_set.all_cubes():
+        stop = start + len(c.wavelength)
+        output_cube_array[start:stop] = reproject_cube(c, output_projection, ny, nx)
         start = stop
+
+    # stitching here
 
     order = np.argsort(output_wavs)
     output_wavs = output_wavs[order]
@@ -155,7 +188,7 @@ def reproject_and_merge_cubes(
     return output_wavs, output_cube_array
 
 
-def reproject_cube(cube_dict, wcs, ny, nx):
+def reproject_cube(cube, wcs, ny, nx):
     """
     Reproject every slice of cube onto wcs, ny, nx.
 
@@ -163,9 +196,9 @@ def reproject_cube(cube_dict, wcs, ny, nx):
     -------
     output_array: np.ndarray indexed on wavelength, y, x
     """
-    num_wavs = len(cube_dict["wavelength"])
-    input_array = cube_dict["cube"].data
-    input_wcs = cube_dict["wcs"]
+    num_wavs = len(cube.wavelength)
+    input_array = cube.data
+    input_wcs = cube.wcs
 
     output_array = np.zeros((num_wavs, ny, nx))
     for w in range(num_wavs):
@@ -198,6 +231,7 @@ def make_square_aperture_grid(
     return SkyRectangularAperture(positions, size, size)
 
 
+##################################OBSOLETE######################################
 def extract_spectra(cube_dicts, sky_apertures):
     wavtables = [d["wavelength"] for d in cube_dicts]
     cubes = [d["cube"] for d in cube_dicts]
@@ -221,6 +255,9 @@ def extract_spectra(cube_dicts, sky_apertures):
             row_counter += 1
 
     return output
+
+
+##################################OBSOLETE######################################
 
 
 def main():
