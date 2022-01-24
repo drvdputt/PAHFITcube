4682ba64e9c5698e7c63eeb4a4dc784f6b6bbacb
Dries Van De Putte <dvandeputte@stsci.edu>
2022-01-24 17:17:38 -0500

    Script and notebook about trimmed models

---
+++ notebooks/make_trimmed_model.py
@@ -0,0 +1,35 @@
+import numpy as np
+from astropy.table import Table
+from pahfit.base import PAHFITBase
+
+
+def make_trimmed_model(packfile, obsdata):
+    # determine wavelength range
+    w = obsdata["x"].value
+    wmin = np.amin(w)
+    wmax = np.amax(w)
+
+    # Different idea: trim the astropy table first, then continue as normal
+    t = Table.read(packfile, format="ipac")
+
+    # decide which rows we are going to keep
+    keep_row = np.full(len(t), True)
+
+    is_drude_or_gauss = np.logical_or(t["Form"] == "Drude1D", t["Form"] == "Gaussian1D")
+    # Only keep drudes and gauss with center within range. It is called
+    # x0 for both so we can use the same code.
+    feature_x0 = t[is_drude_or_gauss]["x_0"]
+    keep_row[is_drude_or_gauss] = np.logical_and(wmin < feature_x0, feature_x0 < wmax)
+
+    # now parse the trimmed table
+    param_info = PAHFITBase.parse_table(t[keep_row])
+
+    # and create a new model (and don't pass a file name, so that the
+    # current contents of param_info are used)
+    trimmed_model = PAHFITBase(
+        obsdata["x"].value,
+        obsdata["y"].value,
+        estimate_start=True,
+        param_info=param_info,
+    )
+    return trimmed_model
