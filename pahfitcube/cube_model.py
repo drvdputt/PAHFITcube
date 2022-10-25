84f4ab4b27c580f1c597d7c7748ba6bcf1373a7c
Dries Van De Putte <dvandeputte@stsci.edu>
2022-10-25 16:32:48 -0400

    Print message when loading or skipping

--- pahfitcube/cube_model.py
+++ pahfitcube/cube_model.py
@@ -98,33 +98,9 @@ class CubeModel:
                     if model_xy is not None:
                         self._ingest_single_model(model_xy, args["x"], args["y"])
 
-        for x, y in product(range(nx), range(ny)):
-            if all(cube.flux[x, y] == 0) or not all(np.isfinite(cube.flux[x, y])):
-                continue
-            model_xy = None
-
-            try:
-                # with checkpoints
-                if checkpoint_prefix is not None:
-                    fn = f"{checkpoint_prefix}_xy_{x}_{y}.ecsv"
-                    if isfile(fn):
-                        model_xy = Model.from_saved(fn)
-                    else:
-                        model_xy = self.model.copy()
-                        model_xy.fit(cube[x, y], maxiter=maxiter)
-                        model_xy.save(fn)
-
-                # without checkpoints, use simpler logic
-                else:
-                    model_xy = self.model.copy()
-                    model_xy.fit(cube[x, y], maxiter=maxiter)
-                # store all the models for later reference
-                self.models[(x, y)] = model_xy
-                # also write to mapcollection
-                self._ingest_single_model(model_xy, x, y)
-
-            except astropy.modeling.fitting.NonFiniteValueError as e:
-                print(f"Fit failed for pixel {x} {y} due to {e}")
+        else:
+            for x, y in product(range(nx), range(ny)):
+                self._fit_spaxel(cube, x, y, maxiter, checkpoint_prefix)
 
     def _ingest_single_model(self, model, x, y):
         # print("ingesting result")
@@ -133,6 +109,43 @@ class CubeModel:
         flat_result = unique_feature_dict(model)
         for k, v in flat_result.items():
             self.maps[k][x, y] = v
+
+    def _fit_spaxel(self, cube, x, y, maxiter, checkpoint_prefix):
+        spec = cube[x, y]
+        if self._skip(spec):
+            print(f"Skipping ({x, y}), too many bad values")
+            return
+
+        model_xy = None
+        try:
+            # with checkpoints
+            if checkpoint_prefix is not None:
+                fn = f"{checkpoint_prefix}_xy_{x}_{y}.ecsv"
+                if isfile(fn):
+                    print(f"Loading {fn}")
+                    model_xy = Model.from_saved(fn)
+                else:
+                    model_xy = self.model.copy()
+                    model_xy.fit(spec, maxiter=maxiter)
+                    model_xy.save(fn)
+
+            # without checkpoints, use simpler logic
+            else:
+                model_xy = self.model.copy()
+                model_xy.fit(spec, maxiter=maxiter)
+            # store all the models for later reference
+            self.models[(x, y)] = model_xy
+            # also write to mapcollection
+            self._ingest_single_model(model_xy, x, y)
+
+        except astropy.modeling.fitting.NonFiniteValueError as e:
+            print(f"Fit failed for pixel {x} {y} due to {e}")
+
+    def _skip(self, spec):
+        num_wav = len(spec.wavelength)
+        too_many_zeros = np.count_nonzero(spec.flux.value <= 0) > 0.2 * num_wav
+        has_nan = not all(np.isfinite(spec.flux.value))
+        return too_many_zeros or has_nan
 
     def plot_spaxel(self, cube, x, y, save_fn=None):
         plt.figure()
