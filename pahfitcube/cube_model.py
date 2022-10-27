8cf1b47b149a34cfbb402429240c4af5394d3100
Dries Van De Putte <dvandeputte@stsci.edu>
2022-10-27 12:13:43 -0400

    Move map collection to separate module

--- pahfitcube/cube_model.py
+++ pahfitcube/cube_model.py
@@ -3,54 +3,9 @@ from specutils import Spectrum1D
 from pahfit.model import Model
 import numpy as np
 from os.path import isfile
-from astropy.io import fits
-from matplotlib import pyplot as plt
 import astropy
 from multiprocess.pool import Pool
 from tqdm import tqdm
-
-
-class MapCollection:
-    """Object representing a collection of nd named maps with the same wcs."""
-
-    def __init__(self, names, shape):
-        """
-        names: list of str
-
-        shape: nx, ny
-        """
-        self.names = names
-        self.data = np.zeros((len(names), *shape))
-        self.index = {name: i for i, name in enumerate(self.names)}
-
-    def __getitem__(self, name):
-        """Access one of the maps using [name]"""
-        if name in self.names:
-            return self.data[self.index[name]]
-        else:
-            raise RuntimeError(
-                f"Map of {name} not in map collection\n"
-                "Available names are" + str([k for k in self.index])
-            )
-
-    def plot_map(self, name):
-        plt.figure()
-        array = self[name]
-        vmin = np.amin(array)
-        vmax = np.percentile(array, 99)
-        # imshow assumes (y, x), so transpose
-        plt.imshow(self[name].T, origin="lower", vmin=vmin, vmax=vmax)
-        plt.colorbar()
-
-    def save(self, wcs, fits_fn):
-        """Save to one (or many?) files."""
-        header = wcs.to_header()
-        new_hdul = fits.HDUList()
-        for k in self.index:
-            i = self.index[k]
-            hdu = fits.ImageHDU(data=self.data[i], header=header, name=k)
-            new_hdul.append(hdu)
-        new_hdul.writeto(fits_fn, overwrite=True)
 
 
 def unique_feature_dict(model: Model):
