def2d616db8ca436d83b54bd5a2104c01ce96acc
Dries Van De Putte <dvandeputte@stsci.edu>
2022-11-01 13:16:26 -0400

    Solved problems with parallel processing

--- pahfitcube/cube_model.py
+++ pahfitcube/cube_model.py
@@ -7,6 +7,8 @@ import astropy
 from multiprocess.pool import Pool
 from tqdm import tqdm
 
+from pahfitcube.map_collection import MapCollection
+
 
 def unique_feature_dict(model: Model):
     """Flatten features table into dict of {unique_feature_name: value}"""
@@ -51,78 +53,45 @@ class CubeModel:
         self.maps = MapCollection(self.flat_feature_names, (nx, ny))
         self.models = {}
 
-        if j > 1:
-            self.model.save("./temp.ecsv", overwrite=True)
-            with Pool(j) as p:
-                # generates arguments for wrapper
-                #
-                args_it = (
-                    dict(
-                        x=x,
-                        y=y,
-                        # cube=cube,
-                        model="temp.ecsv",
-                        checkpoint_prefix=checkpoint_prefix,
-                        maxiter=maxiter,
-                    )
-                    for x, y in product(range(nx), range(ny))
-                )
-                # generates models by calling wrapper on arguments
-                results_it = p.imap(wrapper, args_it)
+        # argument generator
+        args_it = (
+            dict(
+                x=x,
+                y=y,
+                model=self.model,
+                checkpoint_prefix=checkpoint_prefix,
+                maxiter=maxiter,
+                spectral_axis=cube.spectral_axis,
+                flux=cube.flux[x, y],
+                uncertainty=cube.uncertainty[x, y],
+                meta=cube.meta,
+            )
+            for x, y in product(range(nx), range(ny))
+        )
 
-                # do the parallel iteration
-                for model_xy, args in zip(results_it, args_it):
-                    if model_xy is not None:
-                        self._ingest_single_model(model_xy, args["x"], args["y"])
+        # Same loop for serial and parallel, just use different iterator
+        def fit_loop(results_it):
+            for x, y, model_xy in tqdm(results_it, total=nx * ny):
+                # print(x, y)
+                self._ingest_single_model(model_xy, x, y)
+
+        if j > 1:
+            with Pool(j) as p:
+                # version with parallel Pool.imap
+                fit_loop(p.imap(wrapper, args_it))
 
         else:
-            for x, y in tqdm(product(range(nx), range(ny))):
-                self._fit_spaxel(cube, x, y, maxiter, checkpoint_prefix)
+            # version with regular loop
+            fit_loop((wrapper(args) for args in args_it))
 
     def _ingest_single_model(self, model, x, y):
         # print("ingesting result")
         # print(model)
-        self.models[(x, y)] = model
-        flat_result = unique_feature_dict(model)
-        for k, v in flat_result.items():
-            self.maps[k][x, y] = v
-
-    def _fit_spaxel(self, cube, x, y, maxiter, checkpoint_prefix):
-        spec = cube[x, y]
-        if self._skip(spec):
-            print(f"Skipping ({x, y}), too many bad values")
-            return
-
-        model_xy = None
-        try:
-            # with checkpoints
-            if checkpoint_prefix is not None:
-                fn = f"{checkpoint_prefix}_xy_{x}_{y}.ecsv"
-                if isfile(fn):
-                    print(f"Loading {fn}")
-                    model_xy = Model.from_saved(fn)
-                else:
-                    model_xy = self.model.copy()
-                    model_xy.fit(spec, maxiter=maxiter)
-                    model_xy.save(fn)
-
-            # without checkpoints, use simpler logic
-            else:
-                model_xy = self.model.copy()
-                model_xy.fit(spec, maxiter=maxiter)
-            # store all the models for later reference
-            self.models[(x, y)] = model_xy
-            # also write to mapcollection
-            self._ingest_single_model(model_xy, x, y)
-
-        except astropy.modeling.fitting.NonFiniteValueError as e:
-            print(f"Fit failed for pixel {x} {y} due to {e}")
-
-    def _skip(self, spec):
-        num_wav = len(spec.wavelength)
-        too_many_zeros = np.count_nonzero(spec.flux.value <= 0) > 0.2 * num_wav
-        has_nan = not all(np.isfinite(spec.flux.value))
-        return too_many_zeros or has_nan
+        if model is not None:
+            self.models[(x, y)] = model
+            flat_result = unique_feature_dict(model)
+            for k, v in flat_result.items():
+                self.maps[k][x, y] = v
 
     def plot_spaxel(self, cube, x, y):
         try:
@@ -134,34 +103,67 @@ class CubeModel:
             return None
 
 
+def _skip(spec):
+    num_wav = len(spec.wavelength)
+    too_many_zeros = np.count_nonzero(spec.flux.value <= 0) > 0.2 * num_wav
+    has_nan = not all(np.isfinite(spec.flux.value))
+    return too_many_zeros or has_nan
+
+
+def _load_fit_save(x, y, spec, model: Model, maxiter, checkpoint_prefix):
+    # check if we have to load or save a model
+    fn = None
+    load = False
+    if checkpoint_prefix is not None:
+        fn = f"{checkpoint_prefix}_xy_{x}_{y}.ecsv"
+        if isfile(fn):
+            load = True
+
+    # get the model by loading or fitting
+    model_xy = None
+    if load:
+        # print(f"Loading {fn}", end="\r")
+        model_xy = Model.from_saved(fn)
+    else:
+        # analyze spectrum and decide if we will skip or not. Can take
+        # about 1s for big spectra, so do this only if we cannot load.
+        if _skip(spec):
+            # print(f"Skipping ({x, y}), too many bad values")
+            return None
+
+        try:
+            # copy so we don't overwrite the initial conditions
+            # model_xy = Modelmodel.copy()
+            model_xy = Model(model.features.copy())
+            try:
+                model_xy.fit(spec, maxiter=maxiter)
+            except TypeError as e:
+                print(f"caught {e} in cube_model fit")
+                print("x, y = ", (x, y))
+                print("spec = ", spec)
+                raise e
+            # save result if checkpoint path was provided
+            if fn is not None:
+                model_xy.save(fn)
+        except astropy.modeling.fitting.NonFiniteValueError as e:
+            print(f"Fit failed for pixel {x} {y} due to {e}")
+
+    return model_xy
+
+
 def wrapper(args):
     x = args["x"]
     y = args["y"]
-    cube = args["cube"]
     model = args["model"]
     checkpoint_prefix = args["checkpoint_prefix"]
     maxiter = args["maxiter"]
 
-    if all(cube.flux[x, y] == 0) or not all(np.isfinite(cube.flux[x, y])):
-        return None
+    # Spectrum1D not picklable at the moment, so recreate it here (i.e.,
+    # on the parallel process)
+    spectral_axis = args["spectral_axis"]
+    flux = args["flux"]
+    uncertainty = args["uncertainty"]
+    spec = Spectrum1D(flux, spectral_axis, uncertainty=uncertainty)
+    spec.meta = args["meta"]
 
-    model_xy = None
-    try:
-        # with checkpoints
-        if checkpoint_prefix is not None:
-            fn = f"{checkpoint_prefix}_xy_{x}_{y}.ecsv"
-            if isfile(fn):
-                model_xy = Model.from_saved(fn)
-            else:
-                model_xy = Model.from_saved(model)
-                model_xy.fit(cube[x, y], maxiter=maxiter)
-                model_xy.save(fn)
-        # without checkpoints, use simpler logic
-        else:
-            model_xy = Model.from_saved(model)
-            model_xy.fit(cube[x, y], maxiter=maxiter)
-        # store all the models for later reference
-        return None
-    except astropy.modeling.fitting.NonFiniteValueError as e:
-        print(f"Fit failed for pixel {x} {y} due to {e}")
-        return None
+    return x, y, _load_fit_save(x, y, spec, model, maxiter, checkpoint_prefix)
