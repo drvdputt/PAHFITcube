e657de86cae2d3c128650a7a0f41ab8017f93556
Dries Van De Putte <dvandeputte@stsci.edu>
2022-11-14 13:29:22 -0500

    Rotate and crop options for map collection plot

--- pahfitcube/map_collection.py
+++ pahfitcube/map_collection.py
@@ -4,6 +4,7 @@ from astropy.io import fits
 from matplotlib.colors import LogNorm
 import itertools as it
 from astropy.table import Table
+from scipy import ndimage
 
 
 class MapCollection:
@@ -29,26 +30,91 @@ class MapCollection:
                 "Available names are" + str([k for k in self.index])
             )
 
-    def plot_map(self, name, log_color=False, axes=None):
+    def plot_map(
+        self,
+        name,
+        log_color=False,
+        axes=None,
+        with_title=True,
+        rotate_angle=None,
+        autocrop=False,
+    ):
+        """Utility function dealing with the most common plotting issues"""
         if axes is None:
             plt.figure()
             ax = plt.gca()
         else:
             ax = axes
 
-        array = self[name]
-        vmin = np.amin(array)
-        vmax = np.percentile(array, 99)
+        map_data = self[name]
+        vmin = np.amin(map_data)
+        vmax = np.percentile(map_data, 99)
         kwargs = {"origin": "lower"}
         if log_color:
-            vmin = np.amin(array[array > 0])
+            vmin = np.amin(map_data[map_data > 0])
             kwargs["norm"] = LogNorm(vmin=vmin, vmax=vmax)
         else:
             kwargs["vmin"] = vmin
             kwargs["vmax"] = vmax
+
         # imshow assumes (y, x), so transpose
-        cax = ax.imshow(self[name].T, **kwargs)
+        image_data = self[name].T
+        # rotate if requested
+        if rotate_angle is not None:
+            # rotating causes some artifacts with very small nonzero
+            # values. This messes with the autocrop below. We can cut
+            # these values out by remembering the minimum nonzero data
+            # value, and setting anything below that to zero in the
+            # final image.
+            vmin_nonzero = np.amin(image_data[image_data > 0])
+            image_data = ndimage.rotate(image_data, rotate_angle)
+            image_data[image_data < vmin_nonzero] = 0
+            # remember the rotation matrix for this rotation
+            radians = rotate_angle * np.pi / 180
+            c = np.cos(radians)
+            s = np.sin(radians)
+            rotation_matrix = np.array([[c, -s], [s, c]])
+        else:
+            rotation_matrix = np.identity(2)
+
+        center_after_rotation = np.array(image_data.T.shape) / 2
+
+        if autocrop:
+            keep_i = np.where(np.sum(np.square(image_data), axis=1) > 0)[0]
+            keep_j = np.where(np.sum(np.square(image_data), axis=0) > 0)[0]
+            image_data = image_data[keep_i[0] : keep_i[-1], keep_j[0] : keep_j[-1]]
+            # remember the translation vector caused by the cropping
+            crop_translate_xy = np.array([-keep_j[0], -keep_i[0]])
+        else:
+            crop_translate_xy = np.zeros(2)
+
+        cax = ax.imshow(image_data, **kwargs)
         ax.figure.colorbar(cax, ax=ax)
+
+        if with_title:
+            ax.set_title(name)
+
+        # define a function which can be used to convert the displayed
+        # XY to the XY of the original cube
+        def transform_imagexy_to_mapxy(x, y):
+            new_xy = np.array([x, y])
+            print("start", new_xy)
+            # undo the translation due to cropping
+            undo_crop_xy = new_xy - crop_translate_xy
+            print("undo crop", undo_crop_xy)
+            # shift from center of frame to origin
+            origin_xy = undo_crop_xy - center_after_rotation
+            print("to origin", origin_xy)
+            # apply inverted rotation matrix
+            rot_origin_xy = rotation_matrix.dot(origin_xy)
+            print("rotated", rot_origin_xy)
+            # shift back to original center position BEFORE rotation
+            center_original = np.array(map_data.shape) / 2
+            final_xy = rot_origin_xy + center_original
+            print("back to center", final_xy)
+            return final_xy
+
+        return transform_imagexy_to_mapxy
 
     def save(self, wcs, fits_fn):
         """Save to one (or many?) files."""
